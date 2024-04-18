60a5a8593c604daa688d252a94eb3a6a694cfbd3
Jatin Sonani <163812641+JatinSonani@users.noreply.github.com>
2024-04-18 10:38:18 +0530

    Update Q-7.c

--- Q-7.c
+++ Q-7.c
@@ -6,36 +6,25 @@
 //  *           *
 //  *           *
 
-#include <stdio.h>
-
-int main() {
-    int rows = 5;
-
-    for (int i = 1; i <= rows; ++i) {
-        // Print spaces before the asterisks
-        for (int space = 1; space <= rows - i; ++space) {
-            printf(" ");
-        }
-
-        // Print asterisks for the first and last rows
-        for (int j = 1; j <= i; ++j) {
-            printf(" * ");
-        }
-
-        // Move to the next line
-        printf("\n");
-    }
-
-    // Print the middle rows with spaces
-    for (int i = 2; i < rows; ++i) {
-        // Print spaces before the asterisks
-        for (int space = 1; space <= rows - i; ++space) {
-            printf("  ");
-        }
-
-        // Print asterisks for the middle rows
-        printf("*\n");
-    }
-
-   
+#include<stdio.h>
+main()
+{
+	int i,j;
+	for(i=1;i<=6;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1 || i==3 || j==1 || j==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	
+	
 }
